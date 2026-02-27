c882e2f11a0dbfc6860e0deee816eda3f86bd045
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-02-27 14:04:15 +0900

    Initial commit

---
+++ docs/make.jl
@@ -0,0 +1,33 @@
+using MyModule
+using Documenter
+using Downloads
+
+assets_dir = joinpath(@__DIR__, "src", "assets")
+mkpath(assets_dir)
+favicon_path = joinpath(assets_dir, "favicon.ico")
+logo_path = joinpath(assets_dir, "logo.png")
+
+Downloads.download("https://github.com/sotashimozono.png", favicon_path)
+Downloads.download("https://github.com/sotashimozono.png", logo_path)
+
+makedocs(;
+    sitename="MyModule.jl",
+    format=Documenter.HTML(;
+        canonical="https://codes.sota-shimozono.com/MyModule.jl/stable/",
+        prettyurls=get(ENV, "CI", "false") == "true",
+        mathengine=MathJax3(
+            Dict(
+                :tex => Dict(
+                    :inlineMath => [["\$", "\$"], ["\\(", "\\)"]],
+                    :tags => "ams",
+                    :packages => ["base", "ams", "autoload", "physics"],
+                ),
+            ),
+        ),
+        assets=["assets/favicon.ico", "assets/custom.css"],
+    ),
+    modules=[MyModule],
+    pages=["Home" => "index.md"],
+)
+
+deploydocs(; repo="github.com/sotashimozono/MyModule.jl.git", devbranch="main")
---
+++ test/runtests.jl
@@ -0,0 +1,34 @@
+ENV["GKSwstype"] = "100"
+
+using MyModule, Test
+const dirs = []
+
+const FIG_BASE = joinpath(pkgdir(MyModule), "docs", "src", "assets")
+const PATHS = Dict()
+mkpath.(values(PATHS))
+
+@testset "tests" begin
+    test_args = copy(ARGS)
+    println("Passed arguments ARGS = $(test_args) to tests.")
+    @time for dir in dirs
+        dirpath = joinpath(@__DIR__, dir)
+        println("\nTest $(dirpath)")
+        files = sort(
+            filter(f -> startswith(f, "test_") && endswith(f, ".jl"), readdir(dirpath))
+        )
+        if isempty(files)
+            println("  No test files found in $(dirpath).")
+            @test false
+        else
+            for f in files
+                @testset "$f" begin
+                    filepath = joinpath(dirpath, f)
+                    @time begin
+                        println("  Including $(filepath)")
+                        include(filepath)
+                    end
+                end
+            end
+        end
+    end
+end
