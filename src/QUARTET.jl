d9c78c0809f0eac8277da730b0375c702f2690a2
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-02-27 07:46:15 +0000

    refactored

--- src/QUARTET.jl
+++ src/QUARTET.jl
@@ -5,9 +5,14 @@ export BoundaryCondition, Infinite, PBC, OBC
 export AbstractQuantity, Quantity
 export fetch
 
+# --- Core Implementation ---
 include("core/alias.jl")
 include("core/type.jl")
 
+# --- Universality Classes ---
 include("universality/E8.jl")
+
+# --- Models ---
+include("model/TFIM.jl")
 
 end # module QUARTET
